3586bb9f79714860cb9f7b23e91ade16eec12e55
Faluvegi Jozsef-Attila <jfaluvegi@pitechplus.com>
2013-09-30 14:12:46 +0300

    Resolved pull request problems.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -3,9 +3,11 @@
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
-class AppKernel extends Kernel {
+class AppKernel extends Kernel 
+{
 
-    public function registerBundles() {
+    public function registerBundles() 
+            {
         $bundles = array(
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
@@ -28,7 +30,8 @@ class AppKernel extends Kernel {
         return $bundles;
     }
 
-    public function registerContainerConfiguration(LoaderInterface $loader) {
+    public function registerContainerConfiguration(LoaderInterface $loader) 
+            {
         $loader->load(__DIR__ . '/config/config_' . $this->getEnvironment() . '.yml');
     }
 
